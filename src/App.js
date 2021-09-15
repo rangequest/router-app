6cfb2ea3e74383433ac237b044debf192750cc03
Range Quest <range.quest@gmail.com>
2021-09-15 14:49:26 +0530

    Programmatic navigation.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -16,10 +16,14 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products} />
-            <Route path="/posts" component={Posts} />
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" render={props => <Products sortBy="newest" {...props} />} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
+            <Redirect from="/messages" to="/posts" />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
