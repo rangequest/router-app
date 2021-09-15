1f0da46c0770434641a2ec99cd537770e29c778d
Range Quest <range.quest@gmail.com>
2021-09-15 15:02:07 +0530

    Nested routing in admin dashboard.

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Sidebar from "./sidebar";
+import Posts from "./posts";
+import Users from "./users";
+import { Route } from "react-router-dom";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <Sidebar />
+      <Route path="/admin/posts" component={Posts} />
+      <Route path="/admin/users" component={Users} />
     </div>
   );
 };
